a04ff767f653a55489f211cc69d74edb9fcbe993
Renato <renato.mandic@gmail.com>
2017-01-20 14:35:31 +0100

    renamed components, implemented state for AddTeams

--- src/index.js
+++ src/index.js
@@ -5,15 +5,20 @@ import './index.css';
 import 'font-awesome/css/font-awesome.min.css';
 
 // Import main components
-import Home from './Home';
-import GameSession from './GameSession';
+// import Home from './Home';
+import AddTeams from './AddTeams';
+import Session from './Session';
+import GuessedWords from './GuessedWords';
 
 const Root = () => {
   return (
     <BrowserRouter>
       <div>
-        <Match exactly pattern="/" component={Home} />
-        <Match exactly pattern="/game/" component={GameSession} />
+        <Match exactly pattern="/" component={AddTeams} />
+        {/* <Match exactly pattern="/teams" component={AddTeams} /> */}
+        <Match exactly pattern="/game" component={Session} />
+        <Match exactly pattern="/stats" component={GuessedWords} />
+
       </div>
     </BrowserRouter>
   )
